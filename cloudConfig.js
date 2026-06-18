bee99899606e8f3a76b1c95871184bd47cddcf83
Ananya <ananya.singh07042004@gmail.com>
2026-06-18 20:26:10 +0530

    fix: cloudinary storage v2 compatibility

--- cloudConfig.js
+++ cloudConfig.js
@@ -1,21 +1,18 @@
-const cloudinary = require("cloudinary").v2;
-const  CloudinaryStorage  = require("multer-storage-cloudinary");
+const cloudinary = require('cloudinary');
+const cloudinaryStorage = require('multer-storage-cloudinary');
 
-cloudinary.config({
+cloudinary.v2.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUD_API_KEY,
   api_secret: process.env.CLOUD_API_SECRET,
 });
 
-const storage =  CloudinaryStorage({
-  cloudinary,
+const storage = cloudinaryStorage({
+  cloudinary: cloudinary,
   params: {
-    folder: "wanderlust_DEV",
-    allowed_formats: ["png", "jpg", "jpeg"],
+    folder: 'wanderlust_DEV',
+    allowedFormats: ['png', 'jpg', 'jpeg'],
   },
 });
 
-module.exports = {
-  cloudinary,
-  storage,
-};
+module.exports = { cloudinary, storage };
